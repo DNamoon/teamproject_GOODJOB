edc971c1c6c84ce8c03243c6994242af73e051cd
woon1 <woon1998@naver.com>
2022-09-30 17:00:41 +0900

    GOODJOB update v1.0

---
+++ src/main/java/com/example/team_goodjob/TeamGoodjobApplication.java
@@ -0,0 +1,13 @@
+package com.example.team_goodjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamGoodjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamGoodjobApplication.class, args);
+    }
+
+}
