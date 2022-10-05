22a676178b7673fece199d58f6fe3bb01ae69669
DNamoon <leekoo3086@naver.com>
2022-10-05 19:36:01 +0900

    GOODJOB HO branch update v1.2

---
+++ src/main/resources/sql/comdiv.sql
@@ -0,0 +1,6 @@
+insert into comdiv(comdivCode, comdivName) VALUE ('Major','대기업');
+insert into comdiv(comdivCode, comdivName) VALUE ('Middle','중견기업');
+insert into comdiv(comdivCode, comdivName) VALUE ('SMEs','중소기업');
+insert into comdiv(comdivCode, comdivName) VALUE ('StartUp','스타트업');
+insert into comdiv(comdivCode, comdivName) VALUE ('Foreign','외국계기업');
+insert into comdiv(comdivCode, comdivName) VALUE ('Public','국내 공공기관/공기업');
---
+++ src/test/java/com/goodjob/company/service/CompanyServiceTest.java
@@ -0,0 +1,34 @@
+package com.goodjob.company.service;
+
+import com.goodjob.company.Comdiv;
+import com.goodjob.company.Region;
+import com.goodjob.company.dto.CompanyDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class CompanyServiceTest {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @Test
+    public void comRegi() {
+        CompanyDTO companyDTO = CompanyDTO.builder()
+                .comName("moon")
+                .comPw("1234")
+                .comLoginId("test4")
+                .comComdivCode(new Comdiv("StartUp","스타트업"))
+                .comEmail("moon@moon.com")
+                .comPhone("010-9999-1111")
+                .comTerms("1")
+                .build();
+        System.out.println("companyDTO= " + companyDTO.toString());
+        Long c = companyService.createCompanyUser(companyDTO);
+        System.out.println("c = " + c);
+
+    }
+
+}
