f98f8bcbb851fb9dc6c0fa50eaae405b3ce3e5e7
DNamoon <leekoo3086@naver.com>
2022-10-06 15:48:33 +0900

    GOODJOB HO branch update v1.4.1(special_all)

--- src/main/java/com/goodjob/company/CompanyController.java
+++ src/main/java/com/goodjob/company/CompanyController.java
@@ -1,6 +1,7 @@
 /**
  * HO - 2022.10.05
  * 기업 회원가입  & 로그인 컨트롤러
+ * + 2022.10.06 아이디 중복확인 코드 추가 56~65라인
  */
 package com.goodjob.company;
 
@@ -11,9 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -42,6 +41,7 @@ public class CompanyController {
         }
         //회원가입시 비밀번호, 비밀번호확인이 동일하지 않을시 회원가입버튼을 눌러도 회원가입이 되지 않도록 하는 코드
         //result.rejectValue의 field는 DTO의 필드, errorCode는 임의로 지정, defaultMessage는 보여줄 메시지
+        //defaultMessage는 form_errors.html에서 작성한 ${err}에 나타난다.
         if(!companyDTO.getComPw1().equals(companyDTO.getComPw2())){
             result.rejectValue("comPw2","passwordInCorrect",
                     "2개의 패스워드가 일치하지 않습니다.");
@@ -52,6 +52,21 @@ public class CompanyController {
         return "com_register_alert";
     }
 
+    //회원가입 아이디 증복확인시 $.ajax 사용하기 위한 코드
+    @RequestMapping(value = "/comIdCheck", method = RequestMethod.POST)
+    @ResponseBody
+    public String memberIdChkPOST(String comLoginId) throws Exception{
+        int result = companyService.checkId2(comLoginId);
+
+        if(result != 0) {
+            return "fail";	// 중복 아이디가 존재
+        } else {
+            return "success";	// 중복 아이디 x
+        }
+
+    } // memberIdChkPOST() 종료
+
+    //객체 넘기는거 말고
 //    @PostMapping("/com/register")
 //    public String comRegister(CompanyDTO companyDTO) {
 //        System.out.println("companyDTO.toString() = " + companyDTO.toString());
--- src/main/java/com/goodjob/company/CompanyRepository.java
+++ src/main/java/com/goodjob/company/CompanyRepository.java
@@ -1,6 +1,7 @@
 /**
- * 2022.10.22 - HO
- * 회원가입시 아이디 중복확인 시도중(chekId 메서드)
+ * 2022.10.05 - HO
+ * 회원가입시 아이디 중복확인 시도중(checkId 메서드)
+ * ->2022.10.06 checkId2 메서드로 구현.
  */
 package com.goodjob.company;
 
@@ -17,4 +18,7 @@ public interface CompanyRepository extends JpaRepository<Company,Long> {
 
     @Query("select c from Company c where c.comLoginId = :comLoginId")
     String checkId(@Param("comLoginId") String comLoginId);
+
+    @Query("select count(c) from Company c where c.comLoginId =:comLoginId")
+    int checkId2(@Param("comLoginId") String comLoginId);
 }
