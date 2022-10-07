6b3451510a226f72802936e1fade4fc841b4399a
Liakim94 <do942003@gmail.com>
2022-10-07 11:40:42 +0900

    GOODJOB DO2 update v1.0

--- src/main/java/com/goodjob/HomeController.java
+++ src/main/java/com/goodjob/HomeController.java
@@ -1,5 +1,11 @@
 package com.goodjob;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,5 +17,6 @@ public class HomeController {
         return "home";
     }
 
-    }
+
+}
 
---
+++ src/main/java/com/goodjob/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.goodjob;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+/**
+ * 김도현 22.10.01 작성
+ * 김도현 22.10.07 수정 (로그인 에러로 filterChain 메소드 수정)
+ */
+@Configuration
+public class SecurityConfig  {
+
+
+    //  비밀번호 암호화 (단방향)
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().disable()			//cors 방지
+                .csrf().disable()			//csrf 방지
+                //                .formLogin().loginPage("/auth/member/login")		//기본 로그인페이지 없애기
+                //                .defaultSuccessUrl("/").and()
+                .authorizeRequests()
+                .antMatchers("/auth/**","/","/resume/**","/admin/**").permitAll() // /auth이하의 주소들은 인증 필요x
+                .antMatchers("/css/**","/js/**").permitAll()
+                .anyRequest().authenticated().and()
+                .headers().frameOptions().disable();
+
+        return http.build();
+    }
+}
