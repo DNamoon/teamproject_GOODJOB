5b93e3e1dd247da17222753d5ee872104e6b9b8a
oh <wcaqihgn@gmail.com>
2022-10-18 14:22:30 +0900

    GOODJOB update v1.7

--- src/main/java/com/goodjob/SecurityConfig.java
+++ src/main/java/com/goodjob/SecurityConfig.java
@@ -3,8 +3,8 @@ package com.goodjob;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
