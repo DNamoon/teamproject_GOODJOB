964ce60101a885211e366d67ee7eec4670cf8384
DNamoon <leekoo3086@naver.com>
2022-10-26 15:43:28 +0900

    GOODJOB HO2 update v1.8

--- src/main/java/com/goodjob/company/CompanyController.java
+++ src/main/java/com/goodjob/company/CompanyController.java
@@ -2,16 +2,15 @@
  * HO - 2022.10.05
  * 기업 회원가입  & 로그인 컨트롤러
  * + 2022.10.06 아이디 중복확인 코드 추가 56~65라인
+ *
+ * +22.10.26 회원가입 네이밍 변경(register -> signup)
  */
 package com.goodjob.company;
 
 import com.goodjob.company.dto.CompanyDTO;
 import com.goodjob.company.service.CompanyService;
-import com.goodjob.member.Member;
-import com.goodjob.member.memDTO.MemberDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,13 +34,8 @@ public class CompanyController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    //@GetMapping("/login")
-    public String loginMember() {
-        return "/company/comLoginForm";
-    }
-
-    @GetMapping("/register")
-    public String comRegisterForm(Model model, HttpServletRequest request) {
+    @GetMapping("/signup")
+    public String companySignUpForm(Model model, HttpServletRequest request) {
         //22.10.11 세션있으면(로그인 되어있으면) 세션 종료 후 회원 가입 페이지로 넘어가도록 설정.
         HttpSession session = request.getSession(false);
 
@@ -50,15 +44,15 @@ public class CompanyController {
         }
 
         model.addAttribute("companyDTO", new CompanyDTO());
-        return "/company/comRegisterForm";
+        return "/company/companySignUpForm";
     }
 
     //회원가입시 돌아가는 로직. 패스워드 일치하지 않으면 회원가입 불가.
-    @PostMapping("/register")
-    public String comRegister(@Valid CompanyDTO companyDTO, BindingResult result) throws Exception {
+    @PostMapping("/signup")
+    public String companySignUp(@Valid CompanyDTO companyDTO, BindingResult result) throws Exception {
         System.out.println("====================" + companyDTO);
         if(result.hasErrors()){
-            return "/company/comRegisterForm";
+            return "/company/companySignUpForm";
         }
         //회원가입시 비밀번호, 비밀번호확인이 동일하지 않을시 회원가입버튼을 눌러도 회원가입이 되지 않도록 하는 코드
         //result.rejectValue의 field는 DTO의 필드, errorCode는 임의로 지정, defaultMessage는 보여줄 메시지
@@ -67,19 +61,19 @@ public class CompanyController {
         if(!companyDTO.getPw().equals(companyDTO.getComPw2())){
             result.rejectValue("comPw2","passwordInCorrect",
                     "2개의 패스워드가 일치하지 않습니다.");
-            return "/company/comRegisterForm";
+            return "/company/companySignUpForm";
         }
 
         //22.10.20 아이디 중복시 에러메시지 + 로그인 폼 반환
         if(companyService.checkId2(companyDTO.getLoginId()) !=0){
             result.rejectValue("loginId","loginIdDuplicated"
             ,"아이디가 중복됩니다. 다른 아이디를 지정하십시오.");
-            return "/company/comRegisterForm";
+            return "/company/companySignUpForm";
         }
 
         System.out.println("companyDTO.toString() = " + companyDTO.toString());
         companyService.createCompanyUser(companyDTO);
-        return "/company/comRegisterView";
+        return "/company/companySignUpView";
     }
 
     //회원가입 아이디 증복확인시 $.ajax 사용하기 위한 코드
@@ -129,7 +123,7 @@ public class CompanyController {
                     return "redirect:/login?error";  //pw가 틀린 경우
                 }
             } else {
-                return "/company/comRegisterForm";  //id가 없는 경우 회원가입 폼으로 이동
+                return "/company/companySignUpForm";  //id가 없는 경우 회원가입 폼으로 이동
             }
         } else {
             return "redirect:/login?error"; //id가 없는 경우
