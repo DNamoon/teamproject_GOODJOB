490093556d1b4aac8a28dfdee7d96c8e886dd08d
oh <wcaqihgn@gmail.com>
2022-11-03 09:20:11 +0900

    GOODJOB update v.221101

---
+++ src/main/java/com/goodjob/admin/customerInquiry/CustomerInquiryPostAnswerDTO.java
@@ -0,0 +1,12 @@
+package com.goodjob.admin.customerInquiry;
+
+import java.util.Date;
+
+public class CustomerInquiryPostAnswerDTO {
+
+    private String inquiryPostAnswerName;
+
+    private String inquiryPostAnswer;
+
+    private Date inquiryPostAnswerDate;
+}
