b2aa4a075759f81050e862f3958b46673bc644da
oh <wcaqihgn@gmail.com>
2022-11-09 21:50:34 +0900

    GOODJOB update v.221109

--- src/main/java/com/goodjob/post/controller/PostController.java
+++ src/main/java/com/goodjob/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.goodjob.post.controller;
 
 import com.goodjob.bookmark.service.BookMarkService;
+import com.goodjob.company.Company;
 import com.goodjob.company.service.CompanyService;
 import com.goodjob.member.service.MemberService;
 import com.goodjob.post.Post;
@@ -37,11 +38,11 @@ public class PostController {
     private final FileService fileService;
 
     @GetMapping("/savePost")
-    public String postSaveForm(String redirectedFrom ,HttpServletRequest httpServletRequest, Model model){
+    public String postSaveForm(String redirectedFrom, HttpServletRequest httpServletRequest, Model model) {
 
-        String sessionId = getSessionInfo(httpServletRequest,"sessionId");
-        if(sessionId != null){
-            model.addAttribute("comInfo",postService.getComInfo(sessionId)); // CompanyInfoDTO(회사 주소+이름+사업번호+구분)
+        String sessionId = getSessionInfo(httpServletRequest, "sessionId");
+        if (sessionId != null) {
+            model.addAttribute("comInfo", postService.getComInfo(sessionId)); // CompanyInfoDTO(회사 주소+이름+사업번호+구분)
         } else {
             throw new SessionNotFoundException();
         }
@@ -50,18 +51,19 @@ public class PostController {
         model.addAttribute("redirectedFrom", redirectedFrom);
         return "/post/postInsertForm";
     }
-    @PostMapping(value = "/savePost",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+
+    @PostMapping(value = "/savePost", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseBody
     public ResponseEntity<?> postSave(@Valid @ModelAttribute PostInsertDTO postInsertDTO, HttpServletRequest httpServletRequest) throws IOException {
-        log.info("postInsertDTO : "+postInsertDTO);
-        String sessionId = getSessionInfo(httpServletRequest,"sessionId");
-        if(sessionId != null){
+        log.info("postInsertDTO : " + postInsertDTO);
+        String sessionId = getSessionInfo(httpServletRequest, "sessionId");
+        if (sessionId != null) {
             postInsertDTO.setComLoginId(sessionId);
         } else {
             throw new SessionNotFoundException();
         }
         Long savedPostId = postService.savePost(postInsertDTO);
-        if(savedPostId!=null){
+        if (savedPostId != null) {
             SuccessfulPostVo sp = new SuccessfulPostVo();
             sp.setId(savedPostId);
             return new ResponseEntity<>(sp, HttpStatus.OK);
@@ -71,65 +73,67 @@ public class PostController {
     }
 
     @GetMapping(value = {"/updatePost/{postId}"})
-    public String postUpdate(@PathVariable(name="postId")Long postId, String redirectedFrom, HttpServletRequest httpServletRequest, Model model){
-        String sessionId = getSessionInfo(httpServletRequest,"sessionId");
-        if(sessionId != null){
-            model.addAttribute("comInfo",postService.getComInfo(sessionId)); // CompanyInfoDTO(회사 주소+이름+사업번호+구분)
+    public String postUpdate(@PathVariable(name = "postId") Long postId, String redirectedFrom, HttpServletRequest httpServletRequest, Model model) {
+        String sessionId = getSessionInfo(httpServletRequest, "sessionId");
+        if (sessionId != null) {
+            model.addAttribute("comInfo", postService.getComInfo(sessionId)); // CompanyInfoDTO(회사 주소+이름+사업번호+구분)
         } else {
             throw new SessionNotFoundException();
         }
         PostInsertDTO postInsertDTO = postService.getPostById(postId);
-        model.addAttribute("comInfo",postService.getComInfo(sessionId));
-        model.addAttribute("dto",postInsertDTO);
+        model.addAttribute("comInfo", postService.getComInfo(sessionId));
+        model.addAttribute("dto", postInsertDTO);
         model.addAttribute("occList", postService.getListOccupation()); // 직업 리스트
         model.addAttribute("salaryList", postService.getListSalary()); // 연봉대 리스트
         return "/post/postUpdateForm";
     }
 
 
-
     // 기업 마이 페이지 공고관리 처리 메소드
     @GetMapping(value = {"/comMyPagePost"})
-    public String comMyPagePost(PageRequestDTO pageRequestDTO, HttpServletRequest httpServletRequest, Model model){
-        String sessionType = getSessionInfo(httpServletRequest,"Type");
-        if(sessionType != null){
-            pageRequestDTO.setAuthType(getSessionInfo(httpServletRequest,"Type"));
-            pageRequestDTO.setAuth(getSessionInfo(httpServletRequest,"sessionId"));
+    public String comMyPagePost(PageRequestDTO pageRequestDTO, HttpServletRequest httpServletRequest, Model model) {
+        String sessionType = getSessionInfo(httpServletRequest, "Type");
+        if (sessionType != null) {
+            pageRequestDTO.setAuthType(getSessionInfo(httpServletRequest, "Type"));
+            pageRequestDTO.setAuth(getSessionInfo(httpServletRequest, "sessionId"));
         } else {
             throw new SessionNotFoundException();
         }
         PageResultDTO<Post, PostComMyPageDTO> result = postService.getPagingPostListInComMyPage(pageRequestDTO);
         model.addAttribute("occList", postService.getListOccupation()); // 직업 리스트
         model.addAttribute("salaryList", postService.getListSalary()); // 연봉대 리스트
-        model.addAttribute("result",result);
-        model.addAttribute("sessionLoginId",getSessionInfo(httpServletRequest,"sessionId"));
+        model.addAttribute("result", result);
+        model.addAttribute("sessionLoginId", getSessionInfo(httpServletRequest, "sessionId"));
         return "/post/comMyPagePost";
     }
 
     // postId 값으로 개별 공고를 삭제하는 메소드
-    @DeleteMapping(value={"/deletePost/{postId}"})
+    @DeleteMapping(value = {"/deletePost/{postId}"})
     @ResponseBody
     @Transactional
-    public ResponseEntity<String> deletePost(@PathVariable(name = "postId") Long postId){
+    public ResponseEntity<String> deletePost(@PathVariable(name = "postId") Long postId) {
         postService.deletePost(postId);
         return new ResponseEntity<>("Deleted successfully", HttpStatus.OK);
     }
+
     // postId 값으로 개별 공고를 조회하는 페이지로 이동하는 메소드
     @GetMapping(value = {"/readPost/{postId}"})
     @Transactional
-    public String readPost(@PathVariable(name = "postId") Long postId, PageRequestDTO pageRequestDTO, Model model,@SessionAttribute(required = false,name = "sessionId")String sessionId, HttpServletRequest httpServletRequest) throws IOException {
+    public String readPost(@PathVariable(name = "postId") Long postId, PageRequestDTO pageRequestDTO, Model model, @SessionAttribute(required = false, name = "sessionId") String sessionId, HttpServletRequest httpServletRequest) throws IOException {
         // 오성훈 추가
         boolean existsBookmarkByMember = bookMarkService.existsPostByMember(sessionId, postId);
-        model.addAttribute("existsBookmarkByMember",existsBookmarkByMember);
+        model.addAttribute("existsBookmarkByMember", existsBookmarkByMember);
+        boolean existsCompanyOfPostIn = existsCompanyOfPostIn(postId);
+        model.addAttribute("existsPostInCompany",existsCompanyOfPostIn);
 
         PostDetailsDTO postDetailsDTO = postService.readPost(postId);
-        model.addAttribute("dto",postDetailsDTO);
+        model.addAttribute("dto", postDetailsDTO);
         return "/post/postDetailViewWithMap";
     }
 
-    @GetMapping(value={"/file/{fileName}"})
+    @GetMapping(value = {"/file/{fileName}"})
     public ResponseEntity<?> getFile(@PathVariable String fileName) throws IOException {
-        log.info("fileName : "+fileName);
+        log.info("fileName : " + fileName);
         return fileService.getFile(fileName);
     }
 
@@ -137,23 +141,38 @@ public class PostController {
     // @param typeOrSessionId
     // "type" -> "user" or "company"
     // "sessionId" -> 유저나 기업회원 로그인 ID 값
-    private String getSessionInfo(HttpServletRequest httpServletRequest, String typeOrSessionId){
+    private String getSessionInfo(HttpServletRequest httpServletRequest, String typeOrSessionId) {
         HttpSession httpSession = httpServletRequest.getSession(false);
-        if(httpSession != null && httpSession.getAttribute("Type").toString().equals("company")){
+        if (httpSession != null && httpSession.getAttribute("Type").toString().equals("company")) {
             // 세션 타입 체크
             if (typeOrSessionId.equals("Type")) {
-                System.out.println((String)httpSession.getAttribute("Type"));
+                System.out.println((String) httpSession.getAttribute("Type"));
                 System.out.println(httpSession.getAttribute("Type").toString());
                 return httpSession.getAttribute("Type").toString();
             } else if (typeOrSessionId.equals("sessionId")) {
-                System.out.println((String)httpSession.getAttribute("sessionId"));
+                System.out.println((String) httpSession.getAttribute("sessionId"));
                 System.out.println(httpSession.getAttribute("sessionId").toString());
                 return httpSession.getAttribute("sessionId").toString();
             } else {
                 throw new SessionCompanyAccountNotFound();
             }
-        } else{
+        } else {
             return null;
+        }
+    }
+
+    // 22.11.09 오성훈 임시추가. 삭제된 공고인지 확인하는 메소드
+    private boolean existsCompanyOfPostIn(Long postId) {
+        Optional<Post> findOne = postService.findOne(postId);
+        if (findOne.isPresent()) {
+            Company postComId = findOne.get().getPostComId();
+            if (postComId == null) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
         }
     }
 }
